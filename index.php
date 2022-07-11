9a7c221fadcf98d366c8ea574e93ecf1874c82d9
ILGIZ KHUZHAHMETOV <i9270870004@ya.ru>
2022-07-11 18:05:58 +0500

    Перрвый набросок

---
+++ index.php
@@ -0,0 +1,18 @@
+<php 
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SP_PB Шаблон HTML</title>
+    <link rel="icon" type="image/png" href="images/favicon.png">
+<link rel="mask-icon" color="blue" href="images/favicon-apple.svg" >
+<link rel="icon" type="image/svg+xml" href="images/favicon.svg">
+</head>
+<body>
+    <h1>ghbdtn</h1>
+</body>
+</html>
+
